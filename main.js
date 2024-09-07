5fc74cc399171209b5aa934201a2f177385f6ac7
Ayyan Ali Khan <ayyanalikhan510@gmail.com>
2024-09-08 04:46:15 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+const btn = document.querySelector(".btn");
+const section = document.querySelector(".skills");
+btn.addEventListener("click", function () {
+    if (section.style.display !== "none") {
+        section.style.display = "none";
+    }
+    else {
+        section.style.display = "block";
+        btn.style.display = "none"
+    }
+});
---
+++ main.ts
@@ -0,0 +1,11 @@
+const btn = document.querySelector(".btn")
+const section = document.querySelector(".skills")
+
+btn.addEventListener("click",function(){
+    if(section.style.display  !== "none"){
+        section.style.display = "none"
+    }
+    else{
+        section.style.display = "block"
+    }
+})
